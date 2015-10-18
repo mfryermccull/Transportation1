94ea8a966fff41064b0307edd64ac7a9d2ba508e
ShadoWhisps <mfryermccull@umass.edu>
2015-10-18 16:31:35 -0400

    Transportation1
    Transportation project part 1

---
+++ Assets/GameControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+	public GameObject cubePrefab;
+	int numCubes = 16;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < numCubes; i++) {
+			Instantiate (cubePrefab, new Vector3 (i * 2 - 14, 0, 10), Quaternion.identity);
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
